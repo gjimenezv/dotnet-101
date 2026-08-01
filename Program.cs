50cd6961b523f721c4ea2afaf3798a6bb474e69f
Gabriel Jimenez <gabjimenezv@gmail.com>
2026-08-01 12:07:44 -0600

    lesson 4 - connection strings

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// builder.Services.AddDbContext<TuDbContext>(options => 
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+// );
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
