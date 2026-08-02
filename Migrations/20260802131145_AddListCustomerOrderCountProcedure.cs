fef051060f248b2b373ff4ae1cccc1dca6bea67f
Gabriel Jimenez <gabjimenezv@gmail.com>
2026-08-02 07:16:43 -0600

    add procedure for endpoint 6

---
+++ Migrations/20260802131145_AddListCustomerOrderCountProcedure.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_101.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddListCustomerOrderCountProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE ListCostumerOrderCount
+                    @MIN_ORDERS INT
+                AS
+                BEGIN
+                SELECT c.Id AS CustomerId, c.Name AS CustomerName, COUNT(o.Id) as OrdersCount
+                FROM Customers c
+                INNER JOIN Orders o on o.CustomerId = c.Id
+                GROUP BY c.Id, c.Name
+                HAVING COUNT(o.Id) >= @MIN_ORDERS
+                ORDER BY OrdersCount DESC
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE ListCostumerOrderCount");
+
+        }
+    }
+}
